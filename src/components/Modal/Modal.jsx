fc0985eee0f3b8d4f29d8a064b5e493946ab5dff
Valentine <strheadshot1997@gmail.com>
2023-05-21 21:29:09 +0300

    fix problem with dependency: [] in Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,10 +4,12 @@ import { Backdrop, ModalWindow } from './Modal.styled';
 const Modal = ({ closeModal, modalImg }) => {
   useEffect(() => {
     window.addEventListener('keydown', handlePressKey);
+    console.log('Listener add');
     return () => {
       window.removeEventListener('keydown', handlePressKey);
+      console.log('Listener delete');
     };
-  }, []);
+  });
 
   const handlePressKey = e => {
     if (e.code === 'Escape') {
